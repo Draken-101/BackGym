92c2be982875ed4710c7ed98c79ff8eab2d96149
GJZ26 <adolfogj26@gmail.com>
2023-11-16 01:42:42 -0600

    refactor: Modularizing code

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,28 +2,13 @@ import express from 'express'
 import User from '../model/user.model.js';
 import cry from 'bcrypt'
 import dotenv from 'dotenv'
-import { login, register } from '../services/user.services.js';
+import { login, register, remove } from '../services/user.services.js';
 
 dotenv.config()
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
-router.delete('/:id', async function remove(req, res) {
-    console.log(req.params.id)
-    const existing_user = await User.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-
-    if(existing_user === null){
-        return res.status(404).send("User not found.");
-    }
-
-    await existing_user.destroy()
-
-    return res.status(200).send("User successfully deleted")
-})
+router.delete('/:id', remove)
 
 export default router;
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -61,3 +61,20 @@ export async function login(req, res) {
 
     return res.status(200).send("Successful authentication.")
 }
+
+export async function remove(req, res) {
+    console.log(req.params.id)
+    const existing_user = await User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    if (existing_user === null) {
+        return res.status(404).send("User not found.");
+    }
+
+    await existing_user.destroy()
+
+    return res.status(200).send("User successfully deleted")
+}
